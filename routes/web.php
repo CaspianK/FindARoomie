cb81a3e02b96cc02e5a88b2ebc40f6cbb4d4ed46
CaspianK <shalkar.nyn@gmail.com>
2021-04-12 02:51:00 +0600

    Upd routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BookmarkController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoomController;
+use App\Models\City;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [RoomController::class, 'index'])->name('home');
 
 Route::get('/room/create', [RoomController::class, 'create'])->middleware(['auth']);
 Route::post('/room/create', [RoomController::class, 'store'])->middleware(['auth'])->name('room.store');
@@ -26,7 +25,14 @@ Route::get('/room/{id}', [RoomController::class, 'show']);
 Route::get('/profile/create', [ProfileController::class, 'create'])->middleware(['auth']);
 Route::post('/profile/create', [ProfileController::class, 'store'])->middleware(['auth'])->name('profile.store');
 Route::get('/profile/{id}', [ProfileController::class, 'show'])->middleware(['auth']);
+Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth');
 Route::get('/bookmark/create/{room_id}', [BookmarkController::class, 'store'])->middleware(['auth'])->name('bookmark.store');
+
+Route::get('/ast', function() {
+    City::create([
+        'name' => 'Almaty',
+    ]);
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
