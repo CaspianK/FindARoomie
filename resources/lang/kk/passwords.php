4763391c7a89ab9621ce367fe7543680826290f5
CaspianK <shalkar.nyn@gmail.com>
2021-04-15 18:55:05 +0600

    Localization added

---
+++ resources/lang/kk/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Sizdiń kupıya sözińiz jańartıldı!',
+    'sent' => 'Kupıya Sözdi jańartıw siltemesi email`ińizge jiberildi!',
+    'throttled' => 'Kaytalaw ücin küte turıńız.',
+    'token' => 'Kupıya Sözdi jańartıw tokeni teris',
+    'user' => "Bul email`men koldanıwcı tabılmadı.",
+
+];
